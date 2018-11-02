9572c56206a388f8695ceab3f5cc016e0cb25070
DStaojinpeng <DStaojinpeng>
2018-11-01 18:10:28 -0700

    爱鲜蜂开始

---
+++ static/home/js/home.js
@@ -0,0 +1,14 @@
+$(function () {
+   $('.home').width(innerWidth)
+
+    var swiper = new Swiper('.swiper-container', {
+        scrollbar: '.swiper-scrollbar',
+        scrollbarHide: true,
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        spaceBetween: 30,
+        grabCursor: true,
+        loop: true,
+        autoplay: 1500,
+    });
+})
